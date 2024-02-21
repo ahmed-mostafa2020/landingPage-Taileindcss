319c3bfce66e00ff86d034681fecd5ab03753319
Ahmed Mostafa <ahmed-mostafa2020>
2024-02-21 19:09:00 +0200

    start header1

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
-export default function Home() {
-  return (
-    <h1 className=" text-lg bg-red-500 text-mainColor opacity-[.9]">ok</h1>
-  );
-}
+import "../styles/global.scss";
+import Header from "@/src/components/Header";
+
+type Props = {};
+
+const Home = (props: Props) => {
+  return <Header />;
+};
+
+export default Home;
