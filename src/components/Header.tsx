f9cd3fea0e42e768e118f9c1e0abb05a03aab29f
Ahmed Mostafa <ahmed-mostafa2020>
2024-02-25 13:07:21 +0200

    w o header1

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,39 @@
+"use client";
+
 import Image from "next/image";
 import "../../styles/global.scss";
 import logo from "../../public/assets/images/logo.svg";
 import Link from "next/link";
+import { useEffect, useRef } from "react";
 
 type Props = {};
 
 const Header = (props: Props) => {
   const links: string[] = ["features", "teams", "signs"];
+
+  const headerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      const header = headerRef.current;
+      if (header) {
+        if (window.scrollY > 100) {
+          header.style.background = "#0c1524";
+          header.style.padding = "20px 0";
+        } else {
+          headerRef.current.style.background = "transparent";
+          headerRef.current.style.padding = "60px 0";
+        }
+      }
+    });
+  }, []);
+
   return (
-    <header>
-      <div className="container pt-[60px] flex justify-center items-center flex-col gap-5 mx-auto sm:flex-row sm:justify-between ">
+    <header
+      ref={headerRef}
+      className="sticky top-0 left-0 transition-all duration-300 ease-linear"
+    >
+      <div className="container flex justify-center items-center flex-col gap-5 mx-auto sm:flex-row sm:justify-between">
         <Link href="./">
           <Image src={logo} alt="logo" />
         </Link>
